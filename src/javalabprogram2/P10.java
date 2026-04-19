a64e5c61c1e67f8268d4a63ff82cdd6e3e5517d6
Rakshitha <Rakshitha@LAPTOP-E5KTQB5V>
2026-04-19 14:50:56 +0530

    commited

---
+++ src/javalabprogram2/P10.java
@@ -0,0 +1,20 @@
+//Linked hash map
+//2.Include the following elements into the LinkedHashMap called programminglanguages and print allof them.
+//("Java", 1995);("Python", 1991);(“JavaScript", 1995);("C++", 1985);
+package javalabprogram2;
+import java.util.*;
+ 
+public class P10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<String,Integer> programminglanguages = new LinkedHashMap<>();
+		programminglanguages.put("Java", 1995);
+		programminglanguages.put("Python", 1991);
+		programminglanguages.put("JavaScript", 1995);
+		programminglanguages.put("C++", 1985);
+		System.out.println(programminglanguages);
+	}
+
+}
---
+++ src/javalabprogram2/P6.java
@@ -0,0 +1,23 @@
+//Tree map
+//2. Write a Java program to traverse / iterate all the keys with the specified value in a TreeMap 
+//(1,”Apple”), (2,”Strawberry”), (3,”Pear”), (4,”Cucumber”), (5,”Grapes”)
+package javalabprogram2;
+ 
+import java.util.*;
+
+
+public class P6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map< String,String> map1 = new TreeMap<>();
+		map1.put( "c1", "Red");
+		map1.put("c2", "Green");
+		map1.put("c3", "Black");
+		map1.put("c4", "White");
+		
+		System.out.println("Whehther it contains c4 or not: "+map1.containsKey("c4"));
+		System.out.println("Whehther it contains c5 or not: "+map1.containsKey("c5"));
+	}
+
+}
